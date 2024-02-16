817f9d59f092e661a081465026ad46139f595030
Sakovich_Igor <nonord1995@gmail.com>
2024-02-16 19:56:44 +0300

    animation

--- src/app/modules/designer-area/shell/shell.component.ts
+++ src/app/modules/designer-area/shell/shell.component.ts
@@ -1,43 +1,61 @@
+import {
+  animate,
+  animateChild,
+  group,
+  query,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
-import {RouterOutlet} from "@angular/router";
-import {animate, animateChild, query, style, transition, trigger} from "@angular/animations";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-shell',
   templateUrl: './shell.component.html',
   styleUrls: ['./shell.component.css'],
   animations: [
-    trigger("routeAnimationsPersonal", [
-      transition("* => *", [
-        query(":leave", animateChild(), {optional:true}),
-        query(":leave", [
-          style({ left: 0, position: 'relative', opacity: 1 }),
-          animate(
-            "100ms linear",
-            style({ opacity: 0 })
-          )
-        ],{optional:true}),
-        query(":enter", [
-          style({ left: '100%', position: 'relative', opacity: 0 }),
-          animate(
-            "200ms linear",
-            style({ left: '0', position: 'relative', opacity: 1})
-          )
-        ], {optional:true}),
-        query(":enter", animateChild(), {optional:true})
-      ])
-    ])
-  ]
+    trigger('routeAnimationsPersonal', [
+      transition('* <=> *', [
+        style({ position: 'relative' }),
+        query(
+          ':enter, :leave',
+          [
+            style({
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+            }),
+          ],
+          { optional: true }
+        ),
+        query(':enter', [style({ left: '100%' })], { optional: true }),
+        query(':leave', animateChild(), { optional: true }),
+        group([
+          query(
+            ':leave',
+            [animate('400ms ease-out', style({ left: '-100%', opacity: 0 }))],
+            { optional: true }
+          ),
+          query(':enter', [animate('500ms ease-out', style({ left: '0%' }))], {
+            optional: true,
+          }),
+          query('@*', animateChild(), { optional: true }),
+        ]),
+      ]),
+    ]),
+  ],
 })
 export class ShellComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
   prepareRoute(outlet: RouterOutlet) {
     return (
-      outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation']
+      outlet &&
+      outlet.activatedRouteData &&
+      outlet.activatedRouteData['animation']
     );
   }
 }
