042cc7665c44dc5fc3a980706f3f4926636d585e
Igor <nonord1995@gmail.com>
2024-10-28 17:32:23 +0300

    project slider

--- src/app/modules/about-company/shell/shell.component.ts
+++ src/app/modules/about-company/shell/shell.component.ts
@@ -1,22 +1,23 @@
-import {ChangeDetectionStrategy, Component, OnInit} from "@angular/core";
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls: ['./shell.component.css'],
-  templateUrl: './shell.component.html'
+  templateUrl: './shell.component.html',
 })
-export class ShellComponent implements OnInit{
+export class ShellComponent implements OnInit {
   constructor() {
-    const script:Element | null = document.querySelector('script[src="assets/main.js"]');
-    if (script){
-      document.body.removeChild(script)
+    const script: Element | null = document.querySelector(
+      'script[src="assets/main.js"]'
+    );
+    if (script) {
+      document.body.removeChild(script);
     }
-    setTimeout(()=>{
+    setTimeout(() => {
       const script = document.createElement('script');
       script.src = 'assets/main.js';
-      document.body.appendChild(script)
-    }, 1000)
+      document.body.appendChild(script);
+    }, 100);
   }
-  ngOnInit() {
-  }
+  ngOnInit() {}
 }
